0bcba18982aba53b85e8fac480e22a5578ddbae8
성태혁 <sthwin@ybtour.co.kr>
2020-07-16 09:23:19 +0900

    공부중

--- ch02-2/src/index.ts
+++ ch02-2/src/index.ts
@@ -5,8 +5,8 @@ import * as R from 'ramda'
 
 const chance = new Chance()
 let persons: IPerson[] = R.range(0, 2)
-                            .map((n: number) => new Person(chance.name(), chance.age()))
-console.log(persons)                            
+    .map((n: number) => new Person(chance.name(), chance.age()))
+console.log(persons)
 
 // const testMakePerson = (): void => {
 //     let jane: IPerson = makePerson('Jane')
@@ -15,3 +15,31 @@ console.log(persons)
 // }
 
 // testMakePerson()
+
+type func = (number) => number
+
+const add = (a: number): func => {
+    const _add: func = (b: number) => {
+        return a + b
+    }
+    return _add;
+}
+
+let fn: func = add(1)
+console.log(fn)
+console.log(fn(2))
+console.log(add(1)(2))
+
+
+
+const multiply = a => b => c => a * b * c
+
+const multiplySpreaded = (a: number) => {
+    return ((b: number) => {
+        return (c: number) => a * b * c
+    })
+}
+
+
+console.log(multiply(1)(2)(3))
+console.log(multiplySpreaded(1)(2)(3))
