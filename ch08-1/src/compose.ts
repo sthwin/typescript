d7da60fedb9dbe71a4ae48e1ceee74da42bd2619
성태혁 <sthwin@ybtour.co.kr>
2020-07-22 18:28:38 +0900

    공부중

---
+++ ch08-1/src/compose.ts
@@ -0,0 +1,4 @@
+export const f = <T>(x: T): string => 'f(${x})'
+export const g = <T>(x: T): string => 'g(${x})'
+export const h = <T>(x: T): string => 'h(${x})'
+
--- ch08-1/src/function-signature.ts
+++ ch08-1/src/function-signature.ts
@@ -1,3 +1,33 @@
 export type FirstOrderFunc<T, R> = (T) => R
 export type SecondOrderFunc<T, R> = (T) => FirstOrderFunc<T, R>
 export type ThirdOrderFunc<T, R> = (T) => SecondOrderFunc<T, R>
+
+export const add: SecondOrderFunc<number, number> =
+    (x: number): FirstOrderFunc<number, number> =>
+        (y: number): number => x + y
+
+
+export const add3: ThirdOrderFunc<number, number> =
+    (x: number): SecondOrderFunc<number, number> =>
+        (y: number): FirstOrderFunc<number, number> =>
+            (z: number): number => x + y + z
+
+export const add2: SecondOrderFunc<number, number> = add3(1)
+
+export const add1: FirstOrderFunc<number, number> = add2(2)
+
+
+console.log(add3(1)(2)(3))
+
+console.log(
+    add1(3),
+    add2(2)(3),
+    add3(1)(2)(3)
+)
+
+
+function add5(x: number): (number) => number  {
+    return function (y: number): number {
+        return x + y
+    }
+}
