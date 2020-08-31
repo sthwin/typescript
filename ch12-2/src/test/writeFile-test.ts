c8033772c707d980f9a4e11da4f4c07d31d761a3
성태혁 <sthwin@ybtour.co.kr>
2020-09-01 08:58:33 +0900

    작성중

---
+++ ch12-2/src/test/writeFile-test.ts
@@ -0,0 +1,12 @@
+import { mkdir } from './../fileApi/mkdir';
+import { writeFile } from './../fileApi/writeFile';
+
+const writeTest = async (filename: string, data: any) => {
+    const result = await writeFile(filename, data)
+    console.log(`write ${result} to ${filename}`)
+}
+
+mkdir('./data')
+    .then(s => writeTest('./data/hello.txt', 'hello world'))
+    .then(s => writeTest('./data/test.json', JSON.stringify({ name: 'Jack', age: 32 }, null, 2)))
+    .catch((e: Error) => console.log(e.message))
